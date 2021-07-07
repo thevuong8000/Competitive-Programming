ef700b6b6356dc587b17a2a365904a0c33de2e42
dante <ducmanh.tran2904@gmail.com>
2021-07-08 00:24:55 +0700

    refactor: Disjointset with class

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -9,74 +9,89 @@
 using namespace std;
 #define endl '\n'
 
-const int N = 1e5 + 5;
-int pa[N]; /* pa[i] = x if x and i are in the same union
-            or if pa[i] < 0 then group containing i has -pa[i] members (or elements) 
-            i.e. int this case, we might consider i as a root of a group */
-int nElem; // number of elements
+class DSU{
+  private:
+    // the number of distinct sets
+    int numSets = 0;
+  public:
+    // The number of elements
+    int nElems;
 
-void init(){
-    /* At the beginning, there're nElem groups and each group has only 1 elements */
-    memset(pa, -1, sizeof(pa));
-}
+    /* pa[i] = x if x and i are in the same union
+      or if pa[i] < 0 then group containing i has -pa[i] elements. */
+    vector<int> pa;
+    
+    DSU(int nElems){
+      this->nElems = nElems;
+      this->numSets = nElems;
 
-int findPa(int elem){ /* find the root of group containing elem */
-    if(pa[elem] < 0) return elem;
-    else return pa[elem] = findPa(pa[elem]);
-}
-
-int findNumElem(int elem){
-    return -pa[findPa(elem)];
-}
-
-bool isSameUnion(int x, int y){
-    return findPa(x) == findPa(y);
-}
-
-void unionMerge(int x, int y){ /* merge 2 group */
-    if(isSameUnion(x, y)) return; 
-
-    int rootX = findPa(x);
-    int rootY = findPa(y);
-
-    /* This step will reduce the complexity to O(log(N)). This's called Union-by-rank. */
-    if(pa[rootX] > pa[rootY]) swap(rootX, rootY);
-
-    pa[rootX] += pa[rootY]; 
-    pa[rootY] = rootX;
-}
-int numGroup(int n){
-    int num_group = 0;
-    for(int i = 0; i < n; i++){
-    	if(pa[i] < 0) num_group++;
+      // Initially, each element is a distince set
+      pa.assign(nElems, -1);
     }
-    return num_group;
-}
 
+    /* find the root of the set containing {elem} */
+    int findPa(int elem){
+      return pa[elem] < 0 ? elem : (pa[elem] = findPa(pa[elem]));
+    }
+
+    /* get size of the set containing {elem} */
+    int getSize(int elem){
+      return -pa[findPa(elem)];
+    }
+
+    /* check if 2 elements in the same union-set */
+    bool isSameUnion(int x, int y){
+      return findPa(x) == findPa(y);
+    }
+
+    /* merge 2 union-set */
+    void merge(int x, int y){ 
+      if(isSameUnion(x, y)) return; 
+
+      int rootX = findPa(x);
+      int rootY = findPa(y);
+
+      this->numSets -= 1;
+
+        /* This step will reduce the complexity to O(log(N)). This's called Union-by-rank. */
+        // if(pa[rootX] > pa[rootY]) swap(rootX, rootY);
+      if(getSize(rootX) < getSize(rootY)) swap(rootX, rootY);
+
+      pa[rootX] += pa[rootY]; 
+      pa[rootY] = rootX;
+    }
+
+    /* get the number of sets */
+    int getNumSets(){
+      return this->numSets; 
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
-    // test
-    nElem = 10;
-    init();
+    int nElems = 10;
+    DSU dsu(nElems);
+    // dsu = new DSU(nElems);
 
-    /* group 1 */
-    unionMerge(1, 4); unionMerge(1, 6); unionMerge(4, 8); unionMerge(6, 7);
+    /* group 1: {1, 4, 6, 7, 8} */
+    dsu.merge(1, 4);
+    dsu.merge(1, 6);
+    dsu.merge(4, 8);
+    dsu.merge(6, 7);
+    //unionMerge(1, 4); unionMerge(1, 6); unionMerge(4, 8); unionMerge(6, 7);
 
-    /* group 2 */
-    unionMerge(2, 9); unionMerge(3, 9);
+    /* group 2: {2, 3, 9} */
+    dsu.merge(2, 9);
+    dsu.merge(3, 9);
+    //unionMerge(2, 9); unionMerge(3, 9);
 
-    /* group 3: only 5 left */
-    
-    // There're 3 groups:
-    // 1) [1, 4, 6, 7, 8]
-    // 2) [2, 3, 9]
-    // 3) [5]
+    /* group 3: {5, 10} */ 
 
-    cout << isSameUnion(4, 7) << endl; // print 1
-    cout << isSameUnion(1, 2) << endl; // print 0
-    cout << findNumElem(6) << endl; // print 5
+    cout << dsu.isSameUnion(4, 7) << endl; // print 1
+    cout << dsu.isSameUnion(1, 2) << endl; // print 0
+    cout << dsu.getSize(6) << endl; // print 5
+    cout << dsu.getNumSets() << endl; // print 3
     return 0;
 }

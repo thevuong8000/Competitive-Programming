0b0b075af24944c3b40ab3e93d57598d37c54e0c
dante <ducmanh.tran2904@gmail.com>
2021-07-08 15:34:51 +0700

    refactor(Combination): functions to Class

--- Math/Combination.cpp
+++ Math/Combination.cpp
@@ -17,43 +17,63 @@ const int mod = 1e9 + 7; /* mod must be prime */
 
 int fact[N + 5]; /* factorial */
 
-void init(){
-    /* factorial init */
-    fact[0] = 1;
-    for(int i = 1; i <= N; i++) fact[i] = (1ll * fact[i - 1] * i) % mod;
-}
+class Calculator{
+private:
+    // best support for calculate with number less than N
+    int N = 0;
+    int mod = 1e9 + 7;
+    vector<int> fact;
 
-int getFactorial(int n){
-	if(n < 0) return -1;
-	if(n <= N) return fact[n];
-	int res = fact[N];
-	for(int i = N + 1; i <= n; i++) res = (1ll * res * i) % mod;
-	return res;
-}
+public:
+    Calculator(){
+        fact.push_back(1);
+    }
 
-int pw(int n, int k){ /* calculate n^k with n and k is integers */
-	if(k < 0) return 0;
-	if(k == 0) return 1;
-	int res = pw(n, k >> 1);
-	res = (1ll * res * res) % mod;
-	if(k & 1) res = (1ll * res * n) % mod;
-	return res;
-}
+    Calculator(int mod): Calculator(){
+        //Calculator();
+        this->mod = mod;
+    }
 
-int C(int n, int k){
-	if(k > n || k < 0 || n < 0) return -1;
-	int res = getFactorial(n);
-	res = (1ll * res * pw(getFactorial(n - k), mod - 2)) % mod;
-	res = (1ll * res * pw(getFactorial(k), mod - 2)) % mod;
-	return res;
-}
+    int getFactorial(int n){
+    	if(n < 0) return -1;
+	    if(n <= N) return fact[n];
+    	int res = fact[N];
+    	for(int i = N + 1; i <= n; i++){
+            fact.push_back((1ll * i * fact.back()) % mod);
+        }
+        this->N = n;
+    	return fact[n];
+    }
+    
+    // get power n^exp
+    int pw(int n, int exp){
+        /* not support decimal or negative {exp} */
+        if(exp < 0) return -1;
+        if(exp == 0) return 1;
+        int res = pw(n, exp >> 1);
+        res = (1ll * res * res) % mod;
+        if(exp & 1) res = (1ll * res * n) % mod;
+        return res;
+    }
+    
+    // get combination nCk
+    int C(int n, int k){
+        if(k > n || k < 0 || n < 0) return -1;
+    	int res = getFactorial(n);
+    	res = (1ll * res * pw(getFactorial(n - k), mod - 2)) % mod;
+    	res = (1ll * res * pw(getFactorial(k), mod - 2)) % mod;
+    	return res;
+    }
+};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
-    init();
-    cout << C(31, 17) << endl; /* 31C17 = 265182525 */
+    
+    //Calculator cal;
+    Calculator cal(1e9 + 9);
+    cout << cal.getFactorial(5) << endl;
+    cout << cal.C(31, 17) << endl; /* 31C17 = 265182525 */
     
     return 0;
 }

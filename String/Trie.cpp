78a6ed485b7da3f5e61c8e0b49e8ffae3d5e25ab
dante <ducmanh.tran2904@gmail.com>
2021-07-09 11:31:22 +0700

    refactor(Trie): Replace array by vector

--- String/Trie.cpp
+++ String/Trie.cpp
@@ -2,50 +2,75 @@
 using namespace std;
 
 class Trie{
-public:
-	Trie* children[26];
+private:
+    int N = 26;
+    vector<Trie*> children;
 	int endOfWord; // count how many words have this
-
-	Trie(){
+public:
+	Trie(int nChars = 26){
+        this->N = nChars;
 		endOfWord = 0;
-		for(int i = 0; i < 26; i++) children[i] = NULL;
+        children.assign(N, NULL);
 	}
 
-
-	void insert(string key){
+    /**
+     * Insert a new string into Trie
+     * 
+     * @param key string to be inserted
+     * @return the number of that string in Trie(inclusive)
+     */
+	int insert(string key){
 		Trie* cur = this;
 		for(char c : key){
 			int nextChar = c - 'a';
-			if(cur -> children[nextChar] == nullptr) cur -> children[nextChar] = new Trie();
-			cur = cur -> children[nextChar];
+			if(!cur->children[nextChar]) cur->children[nextChar] = new Trie();
+			cur = cur->children[nextChar];
 		}
-		cur -> endOfWord++;
+		return ++cur->endOfWord;
 	}
 
+    /**
+     * Count the number of appearance of {key} in Trie
+     *
+     * @param key string to be counted
+     * @return the number of {key} in Trie
+     */
 	int count(string key){
 		Trie* cur = this;
 		for(char c : key){
 			int nextChar = c - 'a';
-			if(cur -> children[nextChar] == nullptr) return 0;
-			cur = cur -> children[nextChar];
+			if(!cur->children[nextChar]) return 0;
+			cur = cur->children[nextChar];
 		}
-		return cur -> endOfWord;
+		return cur->endOfWord;
 	}
 
+    /**
+     * Check if {key} in Trie
+     *
+     * @param key
+     * @return True if {key} in Trie, False otherwise
+     */
 	bool contains(string key){
 		return count(key) != 0;
 	}
 
+    /**
+     * Remove 1 {key} from Trie
+     *
+     * @param key
+     * @return True if {key} in Trie, False otherwise
+     */
 	bool remove(string key){
 		Trie* cur = this;
 		for(char c : key){
 			int nextChar = c - 'a';
-			if(cur -> children[nextChar] == nullptr) return false;
-			cur = cur -> children[nextChar];
+			if(!cur->children[nextChar]) return false;
+			cur = cur->children[nextChar];
 		}
-		if(!cur -> endOfWord) return false;
+		if(!cur->endOfWord) return false;
 
-		cur -> endOfWord --;
+		cur->endOfWord --;
 		return true;
 	}
 
@@ -60,5 +85,5 @@ int main(){
 	cout << myTrie.count("aba") << endl; // print 2
 	myTrie.remove("aba");
 	cout << myTrie.count("aba") << endl; // print 1
-	return 0;
+    return 0;
 }
